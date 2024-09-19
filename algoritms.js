5b906b7fa9350ae70e8d41e9f96a20056ac1b97a
Maxim Afanasev <maximafanasyevtodo@yandex.ru>
2024-09-19 11:12:12 +0300

    upd

--- algoritms.js
+++ algoritms.js
@@ -40,8 +40,53 @@ function insertionSort(a) {
 	return a;
 };
 
+
+// Быстрая сортировка
+
+function quickSort(arr) {
+  if (arr.length < 2) return arr;
+  let pivot = arr[0];
+  const left = [];
+  const right = [];
+    
+  for (let i = 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat(pivot, quickSort(right));
+}
+
 let array = [85, 92, 12, 11, 0, 85, 1, 1, 20, 77, 01, 10.5, 10.15];
 
+console.log(quickSort(array));
 console.log(bubbleSort(array));
 console.log(Sort(array));
 console.log(insertionSort(array));
+
+
+function merge(left, right) {
+    let arr = []
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift())  
+        } else {
+            arr.push(right.shift()) 
+        }
+    }
+    return [ ...arr, ...left, ...right ]
+}
+
+function mergeSort(array) {
+  const half = array.length / 2
+  if(array.length < 2){
+    return array 
+  }
+  const left = array.splice(0, half)
+  return merge(mergeSort(left),mergeSort(array))
+}
+
+
+console.log(mergeSort(array));
