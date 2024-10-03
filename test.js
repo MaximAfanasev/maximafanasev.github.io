79a172c88dcfb5eb3b7871cb6cc97e8c9a1389bd
Maxim Afanasev <maximafanasyevtodo@yandex.ru>
2024-10-03 12:32:55 +0300

    upd

---
+++ test.js
@@ -0,0 +1,20 @@
+// #1
+
+arr = ['a', 'b', 'c']
+console.log(arr)
+console.log(arr)
+console.log(arr)
+
+arr2 = ['a','b','c','d']
+console.log(arr2)
+
+// #2
+
+const arr3 = [1, 2, 3, 4, 5, 6]
+const divider = 2 
+// [2, 4, 6]
+const f = (a,b) => {
+	return 
+}
+
+console.log(f(arr3, divider))
