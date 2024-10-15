3d2c7f3d8933d4d1e7d1734f0ade87de34f52c23
Maxim <maximafanasyevtodo@yandex.ru>
2024-10-15 09:55:58 +0300

    flanegan

---
+++ 1book.js
@@ -0,0 +1,23 @@
+// page 39
+
+// comment
+
+/* comment */
+
+/***
+comment
+***/
+
+let _ =  1
+let $ = 2
+
+let reserved = ['as','async','await','break','case','catch','class','const','continue','debugger','default','delete','do','else','export','extends','false','finaly','for','from','function','get','if','import','in','instanceof','let','new','null','target','of','return','set','static','super','switch','target','this','throw','true','try','typeof','var','void','while','with','yield']
+
+let caf£ = 1; // unicode
+
+const types = ['map','RegExp','Date', 'Error']
+
+let hex = 0xff
+let float = 3.14
+let billion = 1_000_000_000
+console.log(billion)
