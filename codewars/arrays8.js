04ac889258e9d49288644550561cb3effa0814c2
Maxim Afanasev <maximafanasyevtodo@yandex.ru>
2024-10-30 21:07:07 +0300

    game js

--- codewars/arrays8.js
+++ codewars/arrays8.js
@@ -251,3 +251,87 @@ const findAverage = a => {
 
 
 console.log(findAverage(arr18))
+
+
+
+
+
+// x = 1, n = 10 --> [1,2,3,4,5,6,7,8,9,10]
+// x = 2, n = 5  --> [2,4,6,8,10]
+
+let x = 1
+let n = 10
+
+function countBy(x, n) {
+  let z = [];
+  for (let i=1;i<=n;i++) {
+    z.push(x*i)
+  }
+  return z;
+}
+
+console.log(countBy(x,n))
+
+
+
+// [118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106]
+// [118,"u",120,121,"u",98,122,"a",120,106,104,116,113,114,113,120,106]
+
+const arr19 = [118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106]
+
+function isVow(a){
+  for (var i=0; i<a.length ; ++i)
+  {
+    var char = String.fromCharCode(a[i])
+    if ('aeiou'.indexOf(char) !== -1)
+    a[i] = char;
+  }
+  
+  return a;
+}
+
+console.log(isVow(arr19))
+
+
+
+
+
+
+// [1,2,3,4,5] --> 3 
+const arr20 = [1,2,3,4,5]
+
+function getAverage(marks){
+  return Math.floor(marks.reduce((sum, x) => sum + x) / marks.length);
+}
+
+console.log(getAverage(arr20))
+
+
+
+// [1,-4,7,12] => 1 + 7 + 12 = 20
+const arr21 = [1,-4,7,12]
+function positiveSum(arr) {
+   return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+}
+console.log(positiveSum(arr21))
+
+
+
+
+// [1,2,3,4,5], greater_than_9) -> false
+const arr22 = [1,2,3,4,5]
+const ff = function(v){return v<9}
+function all( arr, fun ){
+  return arr.every(fun)
+}
+console.log(all(arr22,ff))
+
+
+// 35231 => [1,3,2,5,3]
+const Str1 = '35231'
+
+function digitize(n) {
+  return String(n).split('').map(Number).reverse()
+}
+
+console.log(digitize(Str1))
