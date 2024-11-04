9ad039887f47441f00f9a06dc330ceaca93d678a
Maxim Afanasev <maximafanasyevtodo@yandex.ru>
2024-11-04 20:58:52 +0300

    upd

--- codewars/8arr.js
+++ codewars/8arr.js
@@ -6,11 +6,10 @@ arr = ['a', 'b', 'c']
 // 'a-b-c'
 // 'a,b,c'
 
-console.log(arr)
-console.log(arr)
-console.log(arr)
-console.log(arr)
-
+console.log(arr.join(','))
+console.log(arr.join(''))
+console.log(arr.join('-'))
+console.log(arr.join())
 
 
 
@@ -18,13 +17,10 @@ const arr2 = [1, 2, 3, 4, 5, 6]
 const divider = 2
 // [2, 4, 6]
 const f = a => {
-	return 
+	return a.filter(x => x%2 === 0)
 }
 
 console.log(f(arr2, divider))
-
-
-
 
 
 // [1, 2, 3] [4, 5, 6] --> 21
@@ -33,7 +29,7 @@ const arr3 = [1, 2, 3]
 const arr4 = [4, 5, 6]
 
 const f2 = (a, b) => {
-	return 
+	return [...a,...b].reduce((a,b)=>a+b)
 }
 
 console.log(f2(arr3, arr4))
@@ -47,15 +43,10 @@ console.log(f2(arr3, arr4))
 const arr5 = [1, 2, 1, 1, 3, 2]
 
 const f3 = a => {
-	return 
+	return [...new Set(a)]
 }
 
 console.log(f3(arr5))
-
-
-
-
-
 
 
 
@@ -63,20 +54,17 @@ console.log(f3(arr5))
 arr6 = ['0', '1', '2', '3', '4', '5']
 
 const f4 = a => {
-	return 
+	return arr6[a%6]
 }
 
 console.log(f4(7))
 
 
 
-
-
-
 // f([1,2,'a']) == '11' (двоичная система)
 
 const f5 = a => {
-	return 
+	return a.filter(x=>typeof x === 'number').reduce((a,b)=>a+b).toString(2)
 }
 
 console.log(f5([1, 2, 'a']))
@@ -91,12 +79,95 @@ console.log(f5([1, 2, 'a']))
 const arr7 = [1, -2, 3, -4, 5]
 
 const f6 = a => {
-	return 
+	return a.map(x=>-x)
 }
 
 console.log(f6(arr7))
 
 
+// "Robin Singh" --> ["Robin", "Singh"]
+const str = "Robin Singh"
+
+const strToArr = a => {
+	return a.split(' ')
+}
+
+console.log(strToArr(str))
+
+
+// [undefined,null,false,true,true,false,null,undefined], 2
+arr11 = [undefined, null, false, true, true, false, null, undefined]
+
+const countSheeps = a => {
+	return a.filter(Boolean).length
+}
+
+console.log(countSheeps(arr11))
+
+
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] --> [1, 3, 5, 7, 9]
+arr12 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const removeEveryOther = a => {
+	return arr.filter((x,i)=>i%2===0)
+}
+
+console.log(removeEveryOther(arr12))
+
+
+// [82, 15, 6, 38, 35]
+// [min, max, diff]
+// [6, 82, -76]
+
+const arr14 = [82, 15, 6, 38, 35]
+
+const differenceInAges = ages => {
+ const m = Math.min(...arr14)
+ const mm = Math.max(...arr14)
+ return [m, mm, m-mm]
+}
+
+console.log(differenceInAges(arr14))
+
+
+
+// [1,3,5,7,9,11,12], [1,2,3,4,5,10,12]
+// [1,2,3,4,5,7,9,10,11,12]
+const arr16 = [1,3,5,7,9,11,12]
+const arr17 = [1,2,3,4,5,10,12]
+
+const mergeArrays = (arr1, arr2) => {
+  const a = [...new Set([...arr1,...arr2])]
+  return a.sort((a,b)=>a-b)
+}
+
+console.log(mergeArrays(arr16, arr17))
+
+
+// [1,2,3,4] --> 2.5
+const arr18 = [1,2,3,4]
+
+const findAverage = a => {
+  return a.reduce((a,b)=>a+b)/a.length
+}
+
+
+console.log(findAverage(arr18))
+
+
+// [1,2,3,4,5] --> 3 
+const arr20 = [1,2,3,4,5]
+
+function getAverage(a){
+  return Math.floor(a.reduce((a,b)=>a+b)/a.length)
+}
+
+console.log(getAverage(arr20))
+
+
+
+
+//////////// REPEAT
 
 
 
@@ -109,61 +180,15 @@ arr9 = ["1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"]
 arr10 = ["0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"]
 
 const points = a => {
-	return 
+	return a.reduce((x,y)=> {
+    return x += y[0] > y[2] ?
+      3 : y[0] < y[2] ? 0 : 1
+  }, 0)
 }
 
 console.log(points(arr8))
 console.log(points(arr9))
 console.log(points(arr10))
-
-
-
-
-
-
-
-
-// "Robin Singh" --> ["Robin", "Singh"]
-const str = "Robin Singh"
-
-const strToArr = a => {
-	return 
-}
-
-console.log(strToArr(str))
-
-
-
-
-
-
-
-// [undefined,null,false,true,true,false,null,undefined], 2
-arr11 = [undefined, null, false, true, true, false, null, undefined]
-
-const countSheeps = a => {
-	return 
-}
-
-console.log(countSheeps(arr11))
-
-
-
-
-
-
-
-// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] --> [1, 3, 5, 7, 9]
-arr12 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-const removeEveryOther = a => {
-	return 
-}
-
-console.log(removeEveryOther(arr12))
-
-
-
 
 
 
@@ -173,25 +198,14 @@ const arr13 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
 
 
 const countPositivesSumNegatives = a => {
-	return 
+	return a.reduce((arr,n)=> {
+    if (n>0) arr[0]++
+    if (n<0) arr[1]+=n
+    return arr
+  }, [0,0])
 }
 
 console.log(countPositivesSumNegatives(arr13))
-
-
-
-
-// [82, 15, 6, 38, 35]
-// [min, max, diff]
-// [6, 82, -76]
-
-const arr14 = [82, 15, 6, 38, 35]
-
-const differenceInAges = ages => {
- return
-}
-
-console.log(differenceInAges(arr14))
 
 
 
@@ -199,44 +213,17 @@ console.log(differenceInAges(arr14))
 // ['Algebra', 'english', 'Geometry', 'history']
 const arr15 = ['Algebra', 'History', 'Geometry', 'English']
 
-const sorter = arr => {
-	return 
+const sorter = a => {
+	return a.sort((a,b)=> {
+    const aa = a.toLowerCase()
+    const bb = b.toLowerCase()
+    if (aa < bb) return 1
+    if (bb > aa) return -1
+    return 0
+  })
 }
 
 console.log(sorter(arr15))
-
-
-
-
-
-
-
-// [1,3,5,7,9,11,12], [1,2,3,4,5,10,12]
-// [1,2,3,4,5,7,9,10,11,12]
-const arr16 = [1,3,5,7,9,11,12]
-const arr17 = [1,2,3,4,5,10,12]
-
-const mergeArrays = (arr1, arr2) => {
- 
-}
-
-console.log(mergeArrays(arr16, arr17))
-
-
-
-
-
-// [1,2,3,4] --> 2.5
-const arr18 = [1,2,3,4]
-
-const findAverage = a => {
-  return 
-}
-
-
-console.log(findAverage(arr18))
-
-
 
 
 
@@ -247,7 +234,11 @@ let x = 1
 let n = 10
 
 function countBy(x, n) {
- 
+  let z = [];
+  for(let i =1; i<=n;i++) {
+    z.push(x*i)
+  }
+  return z;
 }
 
 console.log(countBy(x,n))
@@ -255,12 +246,18 @@ console.log(countBy(x,n))
 
 
 // [118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106]
-// [118,"u",120,121,"u",98,122,"a",120,106,104,116,113,114,113,120,106]
+// [118,"u",120,121,"u",98,122,"a",120,106,104,116,113,114,113,120,106] aeiou
 
 const arr19 = [118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106]
 
 function isVow(a){
- 
+ for (let i=0; i< a.length; i++) {
+   var char = String.fromCharCode(a[i])
+   if ('aeiou'.indexOf(char) !== -1) {
+     a[i] = char
+   }
+ }
+   return a
 }
 
 console.log(isVow(arr19))
@@ -268,23 +265,10 @@ console.log(isVow(arr19))
 
 
 
-
-
-// [1,2,3,4,5] --> 3 
-const arr20 = [1,2,3,4,5]
-
-function getAverage(marks){
-  
-}
-
-console.log(getAverage(arr20))
-
-
-
 // [1,-4,7,12] => 1 + 7 + 12 = 20
 const arr21 = [1,-4,7,12]
-function positiveSum(arr) {
-  
+function positiveSum(a) {
+  return a.reduce((a,b)=>a + (b>0? b : 0))
 }
 console.log(positiveSum(arr21))
 
@@ -294,8 +278,8 @@ console.log(positiveSum(arr21))
 // [1,2,3,4,5], greater_than_9) -> false
 const arr22 = [1,2,3,4,5]
 const ff = function(v){return v<9}
-function all( arr, fun ){
-  
+function all(a, fun ){
+  return a.every(fun)
 }
 console.log(all(arr22,ff))
 
@@ -303,8 +287,9 @@ console.log(all(arr22,ff))
 // 35231 => [1,3,2,5,3]
 const Str1 = '35231'
 
-function digitize(n) {
-  
+function digitize(a) {
+  return a.split('').map(Number).reverse()
 }
 
 console.log(digitize(Str1))
+
