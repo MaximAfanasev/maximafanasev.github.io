19bdbac37af8aceafd8af6e185c452c4ab0fbf41
Maxim <maximafanasyevtodo@yandex.ru>
2024-12-03 09:26:08 +0400

    upd

--- 1.py
+++ 1.py
@@ -45,3 +45,58 @@ print(a3)
 print(divmod(9,5))
 
 print(2**3)
+
+print((-5)**2)
+
+print(0b10)
+print(0o10)
+print(0x10)
+
+print(bin(65))
+print(oct(65))
+print(hex(65))
+
+print(chr(65))
+
+print(int(True))
+
+print(int(98.6))
+
+print(int('99'))
+
+print(int('10',2))
+
+print(True + 2)
+
+print(43+2.)
+
+print(False + 0.)
+
+#comment
+
+a4 = 0
+a4 = 1 + \
+      2
+print(a4)
+
+a5 = 0
+a5 = (1 +
+      2)
+print(a5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
