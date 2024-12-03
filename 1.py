050258529bc007f1bc7d433a7bf28a917fa70acd
Maxim <test@Maxim-2.local>
2024-12-03 07:40:15 +0400

    3.12.2024

--- 1.py
+++ 1.py
@@ -12,3 +12,36 @@ b = 'b'
 print(b, a0[b])
 
 print(type(7))
+
+a1 = b1 = c1 = 2
+print(a1)
+print(b1)
+print(c1)
+
+x = 5
+y = x
+x = 29
+print(x)
+print(y)
+
+a2 = [1,2,3]
+b2 = a2
+a2[0] = 5
+print(b2)
+
+print(bool(1))
+print(bool(0))
+
+print(+123)
+
+print(1_000_000)
+
+a3 = 95
+a3 -= 3
+print(a3)
+a3 /= 3
+print(a3)
+
+print(divmod(9,5))
+
+print(2**3)
