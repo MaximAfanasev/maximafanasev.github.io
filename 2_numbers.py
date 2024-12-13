2583518a95ec1d4c497c77173a668bc4751183bf
Maxim <maximafanasyevtodo@yandex.ru>
2024-12-13 11:00:43 +0400

    13.12.2024

---
+++ 2_numbers.py
@@ -0,0 +1,27 @@
+print(bool(True))
+
+print(bool(1))
+
+print(bool(45))
+
+print(bool(False))
+
+print(bool(0))
+
+print(bool(0.0))
+
+print(9//5)
+
+print(2**2)
+
+print(bin(2))
+
+print(oct(2))
+
+print(hex(2))
+
+print(chr(65))
+
+print(ord('A'))
+
+print(True + 1)
