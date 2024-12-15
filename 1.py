08d9f7942cf8df344b33061f3352fbf6283e8a00
Maxim <maximafanasyevtodo@yandex.ru>
2024-12-15 16:20:37 +0400

    15.12.2024

--- 1.py
+++ 1.py
@@ -1,24 +1,4 @@
-print([1,2,3])
-
-print([1,2,3][2])
-
-print([1,2,3][0:2])
-
-print([1,2,3,4][::2])
-
-arr1 = [1,2,3]
-arr1.append(4)
-print(arr1)
-
-print([1]*3)
-
-arr2 = [1,2,3]
-arr2.extend([4,5])
-print(arr2)
-
-arr3 = [1,2,3]
-arr3 +=[4,5]
-print(arr3)
+print(type('a'))
 
 
 
