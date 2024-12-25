211ae1053cd3e1b45974e833aadcf675f4d3b889
Maxim <maximafanasyevtodo@yandex.ru>
2024-12-25 08:24:00 +0400

    25.12.2024

--- numbers.py
+++ numbers.py
@@ -1,7 +1,3 @@
-
-
-print(bool(True))
-
 print(bool(1))
 
 print(bool(1.2))
@@ -17,3 +13,25 @@ print(bool(0))
 print(bool(0.0))
 
 print(9//5)
+
+print(int(True))
+
+print(int(98.6))
+
+print(int('99'))
+
+print(int('10',2))
+
+print(True + 1)
+
+print(43 + 2.)
+
+print(False + 0.)
+
+print(divmod(9,5))
+
+print(2**3)
+
+print(chr(65))
+
+print(ord('A'))
