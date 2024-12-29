7997c5f6b2582c83933ffc76dc11422240a4f884
Maxim <maximafanasyevtodo@yandex.ru>
2024-12-29 07:31:19 +0400

    29.12.2024

--- py/for.py
+++ py/for.py
@@ -3,9 +3,13 @@ while count <= 5:
     print(count)
     count += 1
 
-
 while True:
     a = input('send q tu quit: ')
     if a == 'q':
+        continue
+    if a == 'qq':
         break
     print(a)
+
+for x in 'abc':
+    print(x)
