221559b449aa415d6ec1e9f5ecc29d20dcb56393
Maxim <maximafanasyevtodo@yandex.ru>
2025-03-05 23:11:27 +0400

    upd

---
+++ num.py
@@ -0,0 +1,50 @@
+a = 7
+print(a)
+b = a
+print(b)
+
+print(123)
+print(1+1)
+print(1-1)
+print(1*2)
+print(+123)
+print(7/2)
+print(7//2)
+print(9//5)
+print(7%3)
+print(9%5)
+print(2**3)
+
+print(1 + \
+2)
+print(1 + 
+2)
+
+c = 5 
+print(c == 7)
+
+print(5 < 5 and 5 < 10 )
+
+print(bool(True))
+print(bool(1))
+print(bool(45))
+print(bool(-45))
+print(bool(False))
+print(bool(0))
+print(bool(0.0))
+
+
+print(chr(65))
+print(ord('A'))
+
+print(int(True))
+print(int(False))
+print(int(98.6))
+print(int('99'))
+print(int('10', 2))
+
+print(float(True))
+
+
+
+
