2fc059b4e9d2a86853e27fd5ea270475e5a0d0ec
Maxim <maximafanasyevtodo@yandex.ru>
2025-03-11 14:28:34 +0400

    upd

---
+++ pythoncode/list.py
@@ -0,0 +1,49 @@
+a = []
+print(a)
+
+b = list()
+print(b)
+
+print(list('abc'))
+
+print(['a','b'][1])
+
+print([1]*3)
+print([1]+[2])
+
+print(['a','b','c'][1:])
+print(['a','b','c'][-1:])
+print(['a','b','c'][:1])
+print(['a','b','c'][:-2])
+
+print('a,b,c'.split(','))
+
+c = ['a','b']
+c.remove('a')
+print(c)
+
+d = [1,2,3]
+d.append(4)
+print(d)
+
+e = [1]
+f = [2]
+e.extend(f)
+print(e)
+
+j = [1,2,3,4]
+j[1:3]=[5,6]
+print(j)
+
+k = [1,2,3]
+k.remove(2)
+print(k)
+
+l = [1,2,3]
+l.pop()
+print(l)
+
+p = [1,2,3]
+p.clear()
+print(p)
+
