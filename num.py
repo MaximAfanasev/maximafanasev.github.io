1e253b45d0116a647b7929dc8d7293f7d4e12479
Maxim <maximafanasyevtodo@yandex.ru>
2025-03-19 17:21:07 +0400

    upd

--- num.py
+++ num.py
@@ -1,34 +1,38 @@
-print(abs(-5)) # 5
+print(abs(-5))           # 5
+print(chr(65))           # A
+print(divmod(10, 3))     # (3, 1)
+print(float(1))          # 1.0
+print(float(True))       # 1.0
+
+print(int(True))         # 1
+print(int(False))        # 0
+print(int(98.6))         # 98
+print(int('99'))         # 99
+print(int('10', 2))      # 2
+
+print(ord('A'))          # 65
+print(pow(2, 3))         # 8
+print(round(3.14159))    #3
+
 print(2.5.as_integer_ratio()) #(5, 2)
+
 a = 10
-print(a.bit_length()) # 4
-print(chr(65)) # A
+print(a.bit_length())    # 4
+
 b = 3 + 4j
-print(b) # 3 + 4j
-print(b.conjugate()) # 3 - 4j
-print(divmod(10, 3))  # (3, 1)
-print(float(1)) # 1.0
+print(b)                 # 3 + 4j
+
+print(b.conjugate())     # 3 - 4j
+
 print(float.fromhex('0x1.ffffp10')) # 2047.984375
-print(float(True)) # 1.0
+
 c = 3 + 4j
-print(c.imag) # 4.0
-print(int(True)) # 1
-print(int(False)) # 0
-print(int(98.6)) # 98
-print(int('99')) # 99
-print(int('10', 2)) # 2
+print(c.imag)            # 4.0
+
 d = 5.0
-print(d.is_integer()) # True
-print(ord('A')) # 65
-print(pow(2, 3)) # 8
-print(c.real) # 3.0
-print(round(3.14159)) #3
+print(d.is_integer())    # True
 
-
-
-
-
-
+print(c.real)            # 3.0
 
 print(1+1)     # 2
 print(+123)    # 123
@@ -40,12 +44,6 @@ print(9//5)    # 1
 print(7%3)     # 1
 print(9%5)     # 4
 print(2**3)    # 8
-
-
-
-
-
-
 
 print(7==5)    # False
 print(5==5)    # True
