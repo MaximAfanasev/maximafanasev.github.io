c0b56b370c01a4f7704370b6d10ec7916e1faefd
Maxim <maximafanasyevtodo@yandex.ru>
2025-03-26 12:37:09 +0400

    upd

---
+++ 0types.py
@@ -0,0 +1,16 @@
+bool         # True
+bytearray
+bytes
+ 
+complex      # 3j
+dict         # {}
+ 
+float        # 1.0
+frozenset
+ 
+int          # 4
+list         # []
+tuple        # ()
+ 
+set
+str          # 3j
--- 1num.py
+++ 1num.py
@@ -6,9 +6,15 @@
 #вызвать код буквы
 #округлить число
 
-#a c d f i o p r
+
+
+#a a c d f i i o p r
+
+
+
 
 print(abs(-5))           # 5
+print(2.5.as_integer_ratio()) #(5, 2)
 print(chr(65))           # A
 print(divmod(10, 3))     # (3, 1)
 print(float(1))          # 1.0
@@ -19,6 +25,9 @@ print(int(False))        # 0
 print(int(98.6))         # 98
 print(int('99'))         # 99
 print(int('10', 2))      # 2
+
+d = 5.0
+print(d.is_integer())    # True
 
 print(ord('A'))          # 65
 print(pow(2, 3))         # 8
@@ -34,12 +43,61 @@ print(round(3.14159))    #3
 
 
 
+print(1+1)     # 2
+print(+123)    # 123
+print(1-1)     # 0
+print(1*2)     # 2
+print(7/2)     # 3.5
+print(7//2)    # 3
+print(9//5)    # 1
+print(7%3)     # 1
+print(9%5)     # 4
+print(2**3)    # 8
 
 
 
 
 
-print(2.5.as_integer_ratio()) #(5, 2)
+
+
+print(7==5)    # False
+print(5==5)    # True
+print(5!=3)    # True
+print(5>3)     # True
+print(5<3)     # False
+print(5>=5)    # True
+print(5<=3)    # False
+
+
+
+
+
+
+
+
+
+
+
+print(5&3)     # 1 (101 & 011 = 001)
+print(5|3)     # 7 (101 | 011 = 111)
+print(5^3)     # 6 (101 ^ 011 = 110)
+print(~5)      # -6
+print(5 << 1)  # 10
+print(5 >> 1)  # 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 a = 10
 print(a.bit_length())    # 4
@@ -54,33 +112,18 @@ print(float.fromhex('0x1.ffffp10')) # 2047.984375
 c = 3 + 4j
 print(c.imag)            # 4.0
 
-d = 5.0
-print(d.is_integer())    # True
 
 print(c.real)            # 3.0
 
-print(1+1)     # 2
-print(+123)    # 123
-print(1-1)     # 0
-print(1*2)     # 2
-print(7/2)     # 3.5
-print(7//2)    # 3
-print(9//5)    # 1
-print(7%3)     # 1
-print(9%5)     # 4
-print(2**3)    # 8
 
-print(7==5)    # False
-print(5==5)    # True
-print(5!=3)    # True
-print(5>3)     # True
-print(5<3)     # False
-print(5>=5)    # True
-print(5<=3)    # False
-print(5&3)     # 1 (101 & 011 = 001)
-print(5|3)     # 7 (101 | 011 = 111)
-print(5^3)     # 6 (101 ^ 011 = 110)
-print(~5)      # -6
-print(5 << 1)  # 10
-print(5 >> 1)  # 2
+
+
+
+
+
+
+
+
+
+
 
