54f476b7dd77b4710f6ae3ded9aca1cb367a29ba
Maxim <maximafanasyevtodo@yandex.ru>
2025-03-26 15:22:40 +0400

    upd

--- 3str.py
+++ 3str.py
@@ -1,30 +1,30 @@
-print('ab'[0])  # 'a'
-print('abc'[:])  # 'abc'
-print('abc'[1:])  # 'bc'
-print('abc'[-2:])  # 'bc'
-print('abcdefj'[::2])  # 'acej'
+print('ab'[0])          # 'a'
+print('abc'[:])         # 'abc'
+print('abc'[1:])        # 'bc'
+print('abc'[-2:])       # 'bc'
+print('abcdefj'[::2])   # 'acej'
 
 text = "Hello"
-print(text[::-1])  # 'olleH'
+print(text[::-1])       # 'olleH'
 
-print(len('abc'))  # 3
+print(len('abc'))       # 3
 
-print('A man.\n P')  # A man.
-                     #  P
+print('A man.\n P')     # A man.
+                        #  P
 
-print(type('a'))  # <class 'str'>
-print(type("b"))  # <class 'str'>
-print(type('''c'''))  # <class 'str'>
+print(type('a'))        # <class 'str'>
+print(type("b"))        # <class 'str'>
+print(type('''c'''))    # <class 'str'>
 
-print(str(98.6))  # '98.6'
+print(str(98.6))        # '98.6'
 
-print(str(1.0e4))  # '10000.0'
+print(str(1.0e4))       # '10000.0'
 
-print(str(True))  # 'True'
+print(str(True))        # 'True'
 
-print('a' + 'b')  # 'ab'
+print('a' + 'b')        # 'ab'
 
-print('a' * 4)  # 'aaaa'
+print('a' * 4)          # 'aaaa'
 
 multi_line = '''Это
 многострочная
@@ -40,7 +40,7 @@ print(f"Меня зовут {name}, мне {age} лет.")
 # Меня зовут Alice, мне 25 лет.
 
 a1 = 'aoiu'
-print('a' in a1)  # True
+print('a' in a1)        # True
 
 
 
@@ -65,42 +65,43 @@ print('a' in a1)  # True
 
 # ccefjiiillrrsss
 
-print('a'.center(3))  # ' a '
+print('a'.center(3))               # ' a '
 
 text = "Hello, world!"
-print(text.count('l'))  # 3
+print(text.count('l'))             # 3
 
-print('a'.endswith('a'))  # True
+print('a'.endswith('a'))           # True
 
-print('{}'.format('str'))  # 'str'
-print('{} {}'.format('Hello', 'world'))  # 'Hello world'
+print('{}'.format('str'))          # 'str'
+print('{} {}'.format('Hello', 'world'))  
+# 'Hello world'
 
-print(''.join(['a', 'b']))  # 'ab'
+print(''.join(['a', 'b']))         # 'ab'
 
-print(','.join(['a', 'b']))  # 'a,b'
+print(','.join(['a', 'b']))        # 'a,b'
 
-print('abc'.isalpha())  # True
-print('abc123'.isalpha())  # False
+print('abc'.isalpha())             # True
+print('abc123'.isalpha())          # False
 
-print('a'.islower())  # True
+print('a'.islower())               # True
 
-print('a'.isupper())  # False
-print('A'.isupper())  # True
+print('a'.isupper())               # False
+print('A'.isupper())               # True
 
-print('a'.ljust(5, '-'))  # 'a----'
-
-text = "  abc  "
-print(text.lstrip())  # 'abc  '
-
-print('ab'.replace('a', 'c'))  # 'cb'
-
-print('a'.rjust(5, '-'))  # '----a'
+print('a'.ljust(5, '-'))           # 'a----'
 
 text = "  abc  "
-print(text.rstrip())  # '  abc'
+print(text.lstrip())               # 'abc  '
 
-print('abc'.split())  # ['abc']
+print('ab'.replace('a', 'c'))      # 'cb'
 
-print('a'.startswith('a'))  # True
+print('a'.rjust(5, '-'))           # '----a'
 
-print(' abc '.strip())  # 'abc'
+text = "  abc  "
+print(text.rstrip())               # '  abc'
+
+print('abc'.split())               # ['abc']
+
+print('a'.startswith('a'))         # True
+
+print(' abc '.strip())             # 'abc'
