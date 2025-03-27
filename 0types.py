453ee4cbfcd3f49c3649f1eb775fa62e1c903fec
Maxim <maximafanasyevtodo@yandex.ru>
2025-03-27 11:01:01 +0400

    upd

--- 0types.py
+++ 0types.py
@@ -1,18 +1,20 @@
-bool         # True
-bytearray
-bytes
- 
-complex      # 3j
-dict         # {}
- 
-float        # 1.0
-frozenset
+print(type(True))                # bool
+print(type(bytearray(b'hh')))    # bytearray
+a = b'aaaa'
+print(type(a))                   # bytes
 
-generate     # (x for x in range())
- 
-int          # 4
-list         # []
-tuple        # ()
- 
-set
-str          # 3j
+print(type(3j))                  # complex
+print(type({}))                  # dict
+
+print(type(1.0))                 # float
+print(type(frozenset[1,2]))      # frozenset
+
+b = (x for x in range(1,2))
+print(type(b))                   # generator
+print(type(4))                   # int
+print(type([]))                  # list
+print(type((1,2)))               # tuple
+
+print(type(set[1,2]))            # set
+print(type('a'))                 # str
+
