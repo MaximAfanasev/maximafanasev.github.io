f4a1efa9378b0dae7bb3a052660152ed595cb833
Maxim <maximafanasyevtodo@yandex.ru>
2025-04-02 07:53:23 +0400

    upd

---
+++ testpy/2bool.py
@@ -0,0 +1,15 @@
+print(bool(True))       # 
+print(bool(1))          # 
+print(bool(45))         # 
+print(bool(-45))        # 
+print(bool([1, 2, 3]))  # 
+print(bool("Hello"))    # 
+print(bool({1: "one"})) # 
+
+print(bool(False))      # 
+print(bool(None))       # 
+print(bool(0))          # 
+print(bool(0.0))        # 
+print(bool({}))         # 
+print(bool([]))         # 
+print(bool(""))         # 
---
+++ testpy/3str.py
@@ -0,0 +1,148 @@
+print('ab'[0])          # 
+print('abc'[:])         # 
+print('abc'[1:])        # 
+print('abc'[-2:])       # 
+print('abcdefj'[::2])   # 
+
+text = "Hello"
+print(text[::-1])       # 
+
+print('a' + 'b')        # 
+print('a' * 4)          # 
+
+
+
+
+
+
+
+
+
+
+
+
+print(len('abc'))       # 
+print('A man.\n P')     # 
+                        #  
+print(type('a'))        # 
+print(type("b"))        # 
+print(type('''c'''))    # 
+
+
+
+
+
+
+
+
+
+
+
+
+multi_line = '''Это
+многострочная
+строка.'''
+print(multi_line)
+
+print("Он сказал: \"Привет!\"")
+# 
+
+name = "Alice"
+age = 25
+print(f"Меня зовут {name}, мне {age} лет.")
+# 
+
+a1 = 'aoiu'
+print('a' in a1)        # 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# cc    efj   iii  ll  rrr  ssss
+
+print('a'.center(3))               # 
+
+text = "Hello, world!"
+print(text.count('l'))             # 
+
+
+
+
+
+
+
+print('a'.endswith('a'))           # 
+
+print('{}'.format('str'))          # 
+print('{} {}'.format('Hello', 'world'))  
+																	 # 
+
+print(''.join(['a', 'b']))         # 
+print(','.join(['a', 'b']))        # 
+
+
+
+
+
+
+
+print('abc'.isalpha())             # 
+print('abc123'.isalpha())          # 
+
+print('a'.islower())               # 
+
+print('a'.isupper())               # 
+print('A'.isupper())               # 
+
+
+
+
+
+
+print('a'.ljust(5, '-'))           # 
+
+text = "  abc  "
+print(text.lstrip())               # 
+
+
+
+
+
+
+print('ab'.replace('a', 'c'))      # 
+
+print('a'.rjust(5, '-'))           # 
+
+text = "  abc  "
+print(text.rstrip())               # 
+
+
+
+
+
+
+
+
+print('abc'.split())               # 
+
+print('a'.startswith('a'))         # 
+
+print(' abc '.strip())             # 
+
+
+print(str(98.6))        # 
+print(str(1.0e4))       # 
+print(str(True))        # 
