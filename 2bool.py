856d7277d4fd3d753e326ed1d3551b628ff2f617
Maxim <maximafanasyevtodo@yandex.ru>
2025-04-11 16:15:52 +0400

    upd

--- 2bool.py
+++ 2bool.py
@@ -2,9 +2,9 @@ print(bool(True))       # True
 print(bool(1))          # True
 print(bool(45))         # True
 print(bool(-45))        # True
-print(bool([1, 2, 3]))  # True
-print(bool("Hello"))    # True
-print(bool({1: "one"})) # True
+print(bool([1, 2]))  # True
+print(bool("x"))    # True
+print(bool({1: "x"})) # True
 
 print(bool(False))      # False
 print(bool(None))       # False
