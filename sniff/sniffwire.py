3cd15233a5bfe5dcffc8764d7c14282b6d0d74c2
Maxim <maximafanasyevtodo@yandex.ru>
2025-05-11 18:07:33 +0400

    upd

--- sniff/sniffwire.py
+++
@@ -1,8 +0,0 @@
-from scapy.all import *
-
-# Захватываем 100 пакетов и сохраняем в файл
-packets = sniff(count=100)
-wrpcap("/Users/test/Documents/maximafanasev.github.io/ositcp/capture.pcap", packets)  # Сохраняем в PCAP
-print("Трафик сохранён в capture.pcap")
-
-# sudo find / -name "capture.pcap" 2>/dev/null
