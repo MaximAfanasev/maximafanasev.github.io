f553c4c628b78a72aae04423cbb2969e75877880
Maxim <maximafanasyevtodo@yandex.ru>
2025-05-24 20:06:01 +0400

    upd

--- js.js
+++ js.js
@@ -3,17 +3,17 @@
 <script src="">
 alert('')
 
-"use strict";
+"use strict"
 
 (function() {
-	'use strict';
+	'use strict'
 })
 
-let a;
-let a = 'Hello';
+let a
+let a = 'Hello'
 
-let $ = 1;
-let _ = 2;
+let $ = 1
+let _ = 2
 
 let 1a; 
 let my-name; 
@@ -21,27 +21,65 @@ let let;
 
 const A = 1;
 
-let a = 3.14;
-1/0 Infinity;
-'a'/2; NaN;
-const bigint = 123n;
-'' "" `` str;
-`Hi, ${name}`;
-let t = true false;
-let a;
-a = undefined;
-typeof;
-Symbol('id');
-null;
+let a = 3.14
+1/0 Infinity
+'a'/2; NaN
+const bigint = 123n
+'' "" `` str
+`Hi, ${name}`
+let t = true false
+let a
+a = undefined
+typeof
+Symbol('id')
+null
 
-let a = prompt(title, 100);
+let a = prompt(title, 100)
 alet(`... ${a}`)
 
-let a = confirm('aaa?');
+let a = confirm('aaa?')
 alert(a)
 
-b = String(a);
-b = Number(a);
-b = Boolean(a);
+b = String(a)
+b = Number(a)
+b = Boolean(a)
 
-x = -x;
+x = -x
+
++
+-
+*
+**
+/
+%
+
+5%2
+2**3
+'a'+'b' 'ab'
+
+let a = 1
++a
+
+a += 2
+
+a++
+++a
+
+2 > 1
+2 == 1
+2 != 1
+'A' > 'B'
+0 == false
+'' == false
+null == undefined
+
+if (2 == 1) {
+	
+} else if (2 != 1) {
+	
+} else {}
+
+let a = 2==1 ? true : false
+
+
+
