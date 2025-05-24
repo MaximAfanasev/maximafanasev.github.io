313d73bbae15290d95ccbd0e2b1a92130a27bf2e
Maxim <maximafanasyevtodo@yandex.ru>
2025-05-24 19:56:52 +0400

    js upd

--- js.js
+++ js.js
@@ -2,3 +2,46 @@
 <script type="text">
 <script src="">
 alert('')
+
+"use strict";
+
+(function() {
+	'use strict';
+})
+
+let a;
+let a = 'Hello';
+
+let $ = 1;
+let _ = 2;
+
+let 1a; 
+let my-name; 
+let let;
+
+const A = 1;
+
+let a = 3.14;
+1/0 Infinity;
+'a'/2; NaN;
+const bigint = 123n;
+'' "" `` str;
+`Hi, ${name}`;
+let t = true false;
+let a;
+a = undefined;
+typeof;
+Symbol('id');
+null;
+
+let a = prompt(title, 100);
+alet(`... ${a}`)
+
+let a = confirm('aaa?');
+alert(a)
+
+b = String(a);
+b = Number(a);
+b = Boolean(a);
+
+x = -x;
