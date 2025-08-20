231bba03a7d352aadaa9bc2a4ec7923e8b56bd89
Maxim <maximafanasyevtodo@yandex.ru>
2025-08-20 14:04:00 +0300

    upd

--- js.js
+++ js.js
@@ -21,32 +21,26 @@ console.log($)
 let _ = 2;
 console.log(_)
 
-const MYBDAY = '16.03.1989';
-console.log(MYBDAY);
-console.log(typeof(MYBDAY));
-
-let number = 123;
-console.log(number)
-console.log(typeof(number))
-
-console.log(1/0);
-console.log(typeof(1/0))
+console.log(typeof('str'), ',typeof str')
+console.log(typeof(123), ',typeof 123')
+console.log(1/0, '1/0');
+console.log(typeof(1/0), ',typeof Infinity')
 
 console.log("не число"/2)
-console.log(typeof("не число"/2), 'type NaN')
+console.log(typeof("не число"/2), ',type NaN')
 
-console.log(typeof(1n), 'type bigint');
+console.log(typeof(1n), ',type bigint')
 
-let str = '1';
-console.log(str);
-console.log("2");
-console.log(`var ${str}`);
+let str = '1'
+console.log(str)
+console.log("2")
+console.log(`var ${str}`)
 
-console.log(`${1 + 2}`);
-console.log(typeof(true), 'typeof true');
+console.log(`${1 + 2}`)
+console.log(typeof(true), 'typeof true')
 console.log(4 > 1, '4 > 1')
 console.log(null, 'null')
-
+console.log(typeof(age))
 
 
 
