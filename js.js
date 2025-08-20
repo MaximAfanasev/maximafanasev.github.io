22d69ccffe07573b8b6f734fdc6570db4a28306d
Maxim <maximafanasyevtodo@yandex.ru>
2025-08-20 15:53:15 +0300

    upd

--- js.js
+++ js.js
@@ -1,42 +1,41 @@
 "use strict";
 
-console.log('Hello world')
+console.log('Hello world', '-----1. Hello world')
 
 console.log(3 +
 	1 +
-	2);
+	2, '-----2. 3 + 1 + 2');
 
-// Com
+// Comment
 
 /* 1
 2 */
 
 let a = 'b';
-console.log(a)
+console.log(a, '-----3. let a');
 a = 'c';
-console.log(a)
+console.log(a, '-----4. let a changed')
 
 let $ = 1;
-console.log($)
+console.log($, '-----5. let $')
 let _ = 2;
-console.log(_)
+console.log(_, '-----6. let _')
 
-console.log(typeof('str'), ',typeof str')
-console.log(typeof(123), ',typeof 123')
-console.log(1/0, ',1/0');
-console.log(typeof(1/0), ',typeof Infinity')
-console.log(typeof("не число"/2), ',type NaN')
-console.log(typeof(1n), ',type bigint')
-console.log(`${1 + 2}`)
-console.log(typeof(true), 'typeof true')
-console.log(4 > 1, '4 > 1')
-console.log(null, 'null')
-console.log(typeof(age))
+console.log(typeof('str'), '-----7. typeof str')
+console.log(typeof(123), '-----8. typeof 123')
+console.log(1/0, '-----9. 1/0');
+console.log(typeof(1/0), '-----10. typeof Infinity')
+console.log(typeof("не число"/2), '-----11. type NaN')
+console.log(typeof(1n), '-----12. type bigint')
+console.log(`${1+2}`, '-----13. ${1+2}')
+console.log(typeof(true), '-----14. typeof true')
+console.log(4 > 1, '-----15. 4 > 1')
+console.log(null, '-----16. null')
+console.log(typeof(age), '-----17. typeof age')
 
 let str = '1'
-console.log(str)
-console.log("2")
-console.log(`var ${str}`)
+console.log(str, '- str = 1')
+console.log(`var ${str}`, '-----18. ${str}')
 
 
 
