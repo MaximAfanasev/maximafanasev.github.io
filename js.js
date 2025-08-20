e3933627563b45198afd34235c1162b34d347c18
Maxim <maximafanasyevtodo@yandex.ru>
2025-08-20 11:58:13 +0300

    upd

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
 "use strict";
 
-alert('');
+console.log('Hello world')
 
-alert(3 +
+console.log(3 +
 	1 +
 	2);
 
@@ -12,5 +12,55 @@ alert(3 +
 2 */
 
 let a = 'b';
+console.log(a)
 a = 'c';
+console.log(a)
+
+let $ = 1;
+console.log($)
+let _ = 2;
+console.log(_)
+
+const MYBDAY = '16.03.1989';
+console.log(MYBDAY);
+console.log(typeof(MYBDAY));
+
+let number = 123;
+console.log(number)
+console.log(typeof(number))
+
+console.log(1/0);
+console.log(typeof(1/0))
+
+console.log("не число"/2)
+console.log(typeof("не число"/2), 'type NaN')
+
+console.log(typeof(1n), 'type bigint');
+
+
+let str = '1';
+console.log(str);
+console.log("2");
+console.log(`var ${str}`);
+
+console.log(`${1 + 2}`);
+console.log(typeof(true), 'typeof true');
+console.log(4 > 1, '4 > 1')
+console.log(null, 'null')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
