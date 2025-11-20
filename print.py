e339750fd05f4aa2258941fdfbb91112ba7ca543
Maxim <maximafanasyevtodo@yandex.ru>
2025-11-20 17:38:23 +0300

    upd

---
+++ print.py
@@ -0,0 +1,34 @@
+print('안녕 Мир')
+print(8*3.57)
+print(5+30*20)
+print(5+30*20/10)
+
+a = 100
+print(a)
+
+a = 200
+b = a
+print(b)
+
+a = 'hi'
+print(a)
+
+a = 'h"hehe"ehe'
+print(a)
+
+a = '''long'''
+print(a)
+
+a = 1000
+b = 'N: %s'
+print(b % a)
+
+a = '%s %s'
+print(a % (0,8))
+
+print(10* 'a')
+
+a = [1,2,3]
+print(a)
+
+
