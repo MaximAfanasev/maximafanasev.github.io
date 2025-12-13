67701a3f6f4679356d70264a4f4ee48a26960ec6
Maxim <maximafanasyevtodo@yandex.ru>
2025-12-13 19:33:32 +0300

    upd

---
+++ 13deque.py
@@ -0,0 +1,10 @@
+def palindrome(word):
+    from collections import deque
+    dq = deque(word)
+    while len(dq) > 1:
+        if dq.popleft() != dq.pop():
+            return False
+    return True
+
+print(palindrome('a'))
+print(palindrome('bbc'))
