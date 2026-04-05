90d7bf98705fa26c79e6a6f0d457bb6c89ba199e
Maxim <maximafanasyevtodo@yandex.ru>
2026-04-05 21:25:35 +0300

    restart

---
+++ 0.py
@@ -0,0 +1,49 @@
+print('a z A Z 0 9')
+a = 95
+print(a)
+print(a-3)
+print(a)
+a = a - 3
+print(a)
+
+print(5)
+print(0)
+#print(05)
+
+print(+123)
+print(-123)
+
+print(5+9)
+print(100-7)
+
+print(4-10)
+print(5+9+3)
+print(4+3-2-1+6)
+print(5+9       +    3)
+print(6*7)
+print(7*6)
+print(6*7*2*3)
+print(9/5)
+
+print(9//5)
+print(9%5)
+print(divmod(9,5))
+
+print(2*3 + 4)
+
+print(0b10)
+print(0o10)
+print(0x10)
+
+print(int(True))
+print(int(False))
+print(int(98.6))
+print(int(1.0e4))
+print(int('99'))
+print(int('-23'))
+print(int('+12'))
+#print(int('99.8'))
+#print(int('99a'))
+print(4+7.0)
+print(True + 2)
+print(False + 5.0)
