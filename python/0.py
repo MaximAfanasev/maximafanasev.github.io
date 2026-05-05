a5b838de0b1f06fa03fca0ba1cb9535b6b51e189
Maxim <maximafanasyevtodo@yandex.ru>
2026-05-05 12:43:12 +0300

    upd

--- python/0.py
+++ python/0.py
@@ -25,3 +25,20 @@ print(100-7)
 print(4-10)
 print(5+9+3)
 print(5+     9 + 3)
+print(6*7)
+
+print(9/5)
+print(9//5)
+print(9%5)
+#print(5/0)
+#print(7//0)
+
+a = 95
+a = a - 3
+print(a)
+
+print(divmod(9,5)) #// %
+
+print(0b10) #2
+print(0o10) #8
+print(0x10) #16
