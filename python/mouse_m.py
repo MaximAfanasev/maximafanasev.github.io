96078807bc33c524817d3eb1f4211b143f84ff0c
Maxim <maximafanasyevtodo@yandex.ru>
2026-05-21 12:50:26 +0300

    checkmate 1 new wordd

---
+++ python/mouse_m.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+pyautogui.moveTo(100, 100, duration=0.25)
+
+#mac os m1
+
+#python3 -m venv path/to/venv
+#source path/to/venv/bin/activate
+#python3 -m pip install pyautogui
+
+#pip3 install pyautogui
