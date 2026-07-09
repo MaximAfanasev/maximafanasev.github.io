1e50b7bd4851f4efe0ad8a7918e4a0cd4f43b7b3
Maxim <maximafanasyevtodo@yandex.ru>
2026-07-09 16:22:34 +0300

    upd

--- 1.py
+++ 1.py
@@ -1,3 +1,25 @@
+a = 7
+print(a)
+
+# 7
+
+b = a
+print(b) 
+
+# 7
+
+a = 1
+z = 1
+A = 1
+Z = 1
+a0 = 1
+a_ = 1
+print(a, z, A, Z, a0, a_) # 1 1 1 1 1 1
+	
+	
+
+
+#0a = 1 Invalid decimal literal
 a = 95
 a = a - 3
 print(a) # 92
