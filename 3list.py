651a746c441870e8eebe816854ede149fdb463cb
Maxim <maximafanasyevtodo@yandex.ru>
2026-07-17 22:53:52 +0300

    upd

---
+++ 3list.py
@@ -0,0 +1,20 @@
+a = []
+print(a)
+
+b = list()
+print(b)
+
+c = ('a', 'b', 'c')
+
+print(list(c))
+
+d = '1/2/4'
+e = d.split('/')
+print(e)
+
+g = 'a/b//c/d///e'
+print(g.split('/'))
+print(g.split('//'))
+
+a = ['a', 'b', 'c']
+print(a[0])
