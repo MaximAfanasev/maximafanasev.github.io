250ecbd77693c6f701aa1a8f2c2e82643c1b32e8
Maxim <maximafanasyevtodo@yandex.ru>
2026-07-20 19:19:55 +0300

    upd

--- 3list.py
+++ 3list.py
@@ -1,20 +1,66 @@
 a = []
 print(a)
+# []
 
 b = list()
 print(b)
+# []
+
+
+
 
 c = ('a', 'b', 'c')
-
 print(list(c))
+# ['a', 'b', 'c']
+
+
+
+
 
 d = '1/2/4'
 e = d.split('/')
 print(e)
+# ['1', '2', '4']
 
 g = 'a/b//c/d///e'
 print(g.split('/'))
 print(g.split('//'))
+# ['a', 'b', '', 'c', 'd', '', '', 'e']
+
+
+
+
 
 a = ['a', 'b', 'c']
-print(a[0])
+print(a[0]) # a
+print(a[1]) # b
+print(a[2]) # c
+
+print(a[-1]) # c
+print(a[-2]) # b
+print(a[-3]) # a
+#print(a[-4]) #IndexError: list index out of range
+
+
+
+
+
+a = ['a', 'b']
+b = ['c', 'd']
+c = ['e', 'f']
+j = [a,b,c]
+
+print(j) # [['a', 'b'], ['c', 'd'], ['e', 'f']]
+print(j[0]) # ['a', 'b']
+print(j[0][1]) # b
+
+
+
+
+
+
+
+
+
+
+
