f6b668068fd0f528d5afec2eb143b57b76235196
Maxim <maximafanasyevtodo@yandex.ru>
2026-07-26 16:29:34 +0300

    added obj py

--- python/objects/objects.py
+++ python/objects/objects.py
@@ -76,11 +76,41 @@ print('a' in a) # True
 
 a = {'a': 1, 'b': 2}
 print(a['a']) # 1
+# print(a['c']) # KeyError
 
 ###########################
 
+a = {'a': 1, 'b': 2}
+print(a.get('a')) # 1
+print(a.get('c', 'nothing')) # nothing
+print(a.get('c')) # None
 
+###########################
 
+a = {'a': 1, 'b': 2}
+print(a.keys()) # dict_keys(['a', 'b'])
 
+###########################
 
+a = {'a': 1, 'b': 2}
+print(a.values()) # dict_values([1, 2])
 
+###########################
+
+a = {'a': 1, 'b': 2}
+print(a.items()) # dict_items([('a', 1), ('b', 2)])
+
+###########################
+
+a = {'a': 1, 'b': 2}
+a['c'] = 3
+print(a) # {'a': 1, 'b': 2, 'c': 3}
+
+###########################
+
+a = {'a': 1, 'b': 2}
+b = a.copy()
+print(b) # {'a': 1, 'b': 2}
+b['c'] = 3
+print(b) # {'a': 1, 'b': 2, 'c': 3}
+print(a) # {'a': 1, 'b': 2}
