64ddc178fdf09d80fa32590810482dcfe39e08fd
Maxim <maximafanasyevtodo@yandex.ru>
2026-08-01 16:04:57 +0300

    added functions

--- python/function/function.py
+++ python/function/function.py
@@ -1,4 +1,16 @@
 def make_a_sound():
     print('quack')
-make_a_sound
+make_a_sound() # quack
 ###########################
+def a():
+    return True
+
+if a():
+    print('quack') # quack
+###########################
+def a(b):
+    return b + '' + b
+print(a('lala')) # lalalalala
+###########################
+
+
