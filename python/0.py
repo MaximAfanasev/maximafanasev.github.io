b9a85db0519b0362b380d52530424622f16cec35
Maxim <maximafanasyevtodo@yandex.ru>
2026-08-04 22:10:43 +0300

    change py files

---
+++ python/0.py
@@ -0,0 +1,53 @@
+a = 7
+print(a) 
+# 7
+
+
+
+b = a
+print(b) 
+# 7
+
+
+
+# a z A Z a0 a_
+# 0a = 1 Invalid decimal literal
+
+
+
+a = 95
+a = a - 3
+print(a) 
+# 92
+
+
+
+a = 95
+temp = a - 3
+a = temp
+print(a) 
+# 92
+
+
+
+a = 95
+a -= 3
+print(a) 
+# 92
+
+
+
+#bbb c d ff g i l t ss
+#bool
+#bytearray
+#bytes
+#complex
+#dict
+#float
+#frozenset
+#generator
+#int
+#list
+#tuple
+#set
+#str
--- python/8.py
+++ python/8.py
@@ -1,17 +1,26 @@
 def make_a_sound():
     print('quack')
 make_a_sound() # quack
-###########################
+
+
+
+
 def a():
     return True
 
 if a():
     print('quack') # quack
-###########################
+
+
+
+
 def a(b):
     return b + '' + b
 print(a('lala')) # lalalalala
-###########################
+
+
+
+
 def a(b):
     if b == 'red':
         print('red')
@@ -20,41 +29,62 @@ def a(b):
     else: print('ololo')
 a('red') # red
 a('aaa') # ololo
-###########################
+
+
+
+
 def a():
     pass
 print(a()) # None
-###########################
+
+
+
+
 def a():
     if None: print('None')
     else: print('Not None')
 a() # Not None
-###########################
+
+
+
+
 def a(x,y,z):
     return {'X': x, 'Y': y, 'Z': z}
 print(a('la', 'va', 'nda'))
 # {'X': 'la', 'Y': 'va', 'Z': 'nda'}
-###########################
+
+
+
+
 def a(x,y,z):
     return {'X': x, 'Y': y, 'Z': z}
 print(a(x='x', y='y', z='z'))
 # {'X': 'x', 'Y': 'y', 'Z': 'z'}
 print(a(x='A', y='B', z='C'))
 # {'X': 'A', 'Y': 'B', 'Z': 'C'}
-###########################
+
+
+
+
 def a(x,y,z='z'):
     return {'X': x, 'Y': y, 'Z': z}
 print(a(x='x', y='y'))
 # {'X': 'x', 'Y': 'y', 'Z': 'z'}
 print(a(x='x', y='y', z='d'))
 # {'X': 'x', 'Y': 'y', 'Z': 'd'}
-###########################
+
+
+
+
 def x(a, b=[]):
     b.append(a)
     print(b)
 x('a') # ['a']
 x('b') # ['a', 'b']
-###########################
+
+
+
+
 def x(a):
     b = []
     b.append(a)
@@ -62,7 +92,10 @@ def x(a):
     return b
 x('a') # ['a']
 x('b') # ['b']
-###########################
+
+
+
+
 def x(a, b=None):
     if b == None:
         b = []
@@ -70,21 +103,33 @@ def x(a, b=None):
     print(b)
     return b
 x('a') # ['a']
-###########################
+
+
+
+
 def x(*args):
     print(args)
 x(a) # (<function a at 0x10caca560>,)
 x('a', 1, 2) # ('a', 1, 2)
-###########################
+
+
+
+
 def x(**kwargs):
     print(kwargs)
 x(a='a', b='b') # {'a': 'a', 'b': 'b'}
-###########################
+
+
+
+
 def x(a):
     '''Documentation'''
     print(a)
 x('b') # b
-###########################
+
+
+
+
 def x(a, b):
     for x in a:
         print(b(x))
@@ -94,7 +139,10 @@ x(['a', 'b', 'c'],y)
 # A!
 # B!
 # C!
-###########################
+
+
+
+
 def x(a, b):
     for x in a:
         print(b(x))
@@ -102,4 +150,6 @@ x(['a', 'b','c'], lambda x: x.capitalize() + '!')
 # A!
 # B!
 # C!
-###########################
+
+
+
